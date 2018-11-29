fe211d9b87d57644c744329e46b7a9527e78554d
ThaymesCoder <tmpofu@thami.co.za>
2018-11-30 01:09:36 +0200

    Implemented the readonly property

--- Classes/Fields/Program.cs
+++ Classes/Fields/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fields
 {
@@ -10,6 +6,13 @@ namespace Fields
     {
         static void Main(string[] args)
         {
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            customer.Promote();
+
+            Console.WriteLine(customer.Orders.Count);
         }
     }
 }
