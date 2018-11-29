3843e66f7e8aa095baad0e62414ad59a5f45ad45
ThaymesCoder <tmpofu@thami.co.za>
2018-11-30 00:47:53 +0200

    Use params,out and ref keywords

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -9,14 +9,55 @@ namespace Classes
 
             //var person = Person.Parse("Thaymes");
             //person.Introduce("Mosh");
-            var customer = new Customer(1,"Thaymes");
+            //var customer = new Customer(1,"Thaymes");
 
 
-            var  order =new Order();
-            customer.Orders.Add(order);
+            //var  order =new Order();
+            //customer.Orders.Add(order);
 
-            Console.WriteLine(customer.Id);
-            Console.WriteLine(customer.Name);
+            //Console.WriteLine(customer.Id);
+            //Console.WriteLine(customer.Name);
+
+            // var number = int.Parse("abc");
+
+
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed.");
+            }
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 5));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5, 6 }));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0},{1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0},{1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured.");
+
+            }
         }
     }
 }
