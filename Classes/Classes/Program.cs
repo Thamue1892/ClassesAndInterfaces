f499e1514edc1939549276f739f9572552517423
ThaymesCoder <tmpofu@thami.co.za>
2018-11-29 07:42:48 +0200

    Class Constructors

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -2,31 +2,21 @@
 
 namespace Classes
 {
-    public class Person
-    {
-        public string Name;
-
-        public void Introduce(string to)
-        {
-            Console.WriteLine("Hi {0}, I am {1}", to, Name);
-        }
-
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.Name = str;
-
-            return person;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            
-            var person = Person.Parse("Thaymes");
-            person.Introduce("Mosh");
+
+            //var person = Person.Parse("Thaymes");
+            //person.Introduce("Mosh");
+            var customer = new Customer(1,"Thaymes");
+
+
+            var  order =new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }
