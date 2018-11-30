e116fa17368a8da1337e59f15eee84b6de1f5fb0
ThaymesCoder <tmpofu@thami.co.za>
2018-11-30 02:02:20 +0200

    Implemented Indexers

---
+++ Classes/AccessModifiers/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthDate)
+        {
+            _birthdate = birthDate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - _birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+
+            }
+        }
+    }
+}
--- Classes/AccessModifiers/Program.cs
+++ Classes/AccessModifiers/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace AccessModifiers
+﻿using System;
+
+namespace AccessModifiers
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1989, 03, 18));
+            Console.WriteLine(person.Age);
         }
     }
 }
