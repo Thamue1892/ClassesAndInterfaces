7d76aece7a63d6f1007335161feefd5cb4d85fab
ThaymesCoder <tmpofu@thami.co.za>
2018-11-30 03:00:03 +0200

    Initial commit

--- Classes/Inheritance/Program.cs
+++ Classes/Inheritance/Program.cs
@@ -2,8 +2,12 @@
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
         }
     }
 }
